d64c33fdc3d6d49437f17cbf4a347bf7761c4ea3
NilsTettschlag <151008289+NilsTettschlag@users.noreply.github.com>
2025-12-23 13:20:32 +0100

    Change Plottng: plot usb datalogger over time

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import seaborn as sns
 from functions import m_data
 
 #set Machine for wich you want to calculate dataoutput
-set_machine = 'DLRA'
+set_machine = 'OCEAN'
 
 path_to_machine_data = f'data/{set_machine}'
 
@@ -29,10 +29,10 @@ data_protocoll = m_data.read_protocoll_file(folder_path_protocoll)
 
 data_timestamps_filtered_datarecorder = m_data.compare_datatimestamps_recorder_protocoll(data_raw_datarecorder, data_protocoll)
 
-data_timestamps_filtered_datarecorder.to_csv(f'output/{set_machine}_data_timestamps_filtered_datarecorder.csv', index=False)
+data_timestamps_filtered_datarecorder.to_csv(f'output/{set_machine}/data_timestamps_filtered_datarecorder.csv', index=False)
 
 data_usb_logger = m_data.read_lascar_file(folder_path_lascar)
 
 data_usb_logger_filtered = m_data.compare_datatimestamps_lascar_protocoll(data_usb_logger, data_protocoll)
 
-data_usb_logger_filtered.to_csv(f'output/{set_machine}_data_usb_logger.csv', index=False)
+data_usb_logger_filtered.to_csv(f'output/{set_machine}/data_usb_logger.csv', index=False)
